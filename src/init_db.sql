896a7d50678365a59d87fb89110c75bc274a914c
Christos Christidis <48795138+CChristidis@users.noreply.github.com>
2022-03-11 17:56:58 +0200

    Update init_db.sql

--- src/init_db.sql
+++ src/init_db.sql
@@ -1,1 +1,28 @@
 CREATE DATABASE mye030;
+USE mye030;
+
+CREATE TABLE country(
+  id INT,
+  name VARCHAR(255),
+  code VARCHAR(255),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE indicator(
+  id INT,
+  name VARCHAR(255),
+  code VARCHAR(255),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE indicates(
+  id_country INT,
+  id_ind INT, 
+  year INT,
+  value FLOAT,
+  PRIMARY KEY(id_country, id_ind, year)
+);
+ 
+  
+
+  
