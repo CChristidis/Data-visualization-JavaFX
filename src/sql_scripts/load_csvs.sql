5d1abd9b23721a831b77e0452c66f37e7f28d5f3
Christos Christidis <48795138+CChristidis@users.noreply.github.com>
2022-03-11 19:13:03 +0200

    Update load_csvs.sql

--- src/sql_scripts/load_csvs.sql
+++ src/sql_scripts/load_csvs.sql
@@ -1,3 +1,4 @@
+-- Load country csv
 LOAD DATA LOCAL INFILE 
   'C:\\Users\\Christos\\Desktop\\MYE30_csv\\country.csv'
 INTO TABLE 
@@ -14,3 +15,23 @@ IGNORE
   1 
 LINES 
   (id, Name, Code);
+
+-- Load indicator csv
+LOAD DATA LOCAL INFILE 
+  'C:\\Users\\Christos\\Desktop\\MYE30_csv\\indicator.csv'
+INTO TABLE 
+  indicator
+CHARACTER SET 
+	latin1 
+FIELDS OPTIONALLY ENCLOSED BY 
+   '"' 
+TERMINATED BY 
+  ','
+LINES TERMINATED BY 
+  '\n' 
+IGNORE 
+  1 
+LINES 
+  (id, Name, Code);
+  
+  
