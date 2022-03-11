351e3646583290c0e7b3eee5e3f81b8b4f957315
Christos Christidis <48795138+CChristidis@users.noreply.github.com>
2022-03-11 23:10:16 +0200

    Update filtering.py

--- src/filtering.py
+++ src/filtering.py
@@ -1,19 +1,60 @@
 import pandas as pd
-import csv
 import numpy
 
-global in_path = ''         # fill
-global out_path = ''        # fill
+in_file = ''                    # countries_data.csv
+out_file = ''                   # year_value.csv
 
-def filter_csv(infile_path: str, outfile_path: str):
-    df = pd.read_csv(infile_path) 
+
+def filter_indicators(in_file: str):
+    df = pd.read_csv(in_file)
     cols = df.columns
-    lst = [df.loc[i] for i in range(len(df)) if (df.loc[i, "Indicator Code"][:2] in ("NY", "SE", "SD"))]
-    arr = numpy.asarray(lst)
+
+    """ Filter all unwanted rows."""
+    indicator_filtering_list = [df.loc[i] for i in range(len(df)) if (df.loc[i, "Indicator Code"][:2] in ("NY", "SE", "SD"))]
+    arr = numpy.asarray(indicator_filtering_list)
     df = pd.DataFrame(arr)
     df.columns = cols
-    df.to_csv(outfile_path)  
-   
+
+    return df
+
+
+def add_countryid_indicatorid(df):
+    countriesdf = df.filter(['Country Name', 'Country Code'])
+    countriesdf = countriesdf.drop_duplicates(subset=None, keep='first', inplace=False)
+    countriesdf['index_country'] = range(1, len(countriesdf) + 1)
+
+    indicatorsdf = df.filter(['Indicator Name', 'Indicator Code'])
+    indicatorsdf = indicatorsdf.drop_duplicates(subset=None, keep='first', inplace=False)
+    indicatorsdf['index_indicat'] = range(1, len(indicatorsdf) + 1)
+
+    dfnew = pd.merge(countriesdf, df, how='inner', on='Country Name')
+    dfnew = dfnew.drop('Country Code_y', 1)
+    dfnew = dfnew.drop('Country Code_x', 1)
+    dfnew = pd.merge(indicatorsdf, dfnew, how='inner', on='Indicator Name')
+    dfnew = dfnew.drop('Indicator Code_y', 1)
+    dfnew = dfnew.rename(columns={'Indicator Code_x': 'Indicator Code', 'Country Code_x': 'Country Code'})
+
+    df_ind_years = dfnew.drop(columns=['Indicator Name', 'Indicator Code', 'Country Name'])
+
+    return df_ind_years
+
+
+def unpivot_df(df):
+    df_unpivoted = df.melt(id_vars=['index_country', 'index_indicat'], var_name='Years', value_name='Values')
+    df_unpivoted.reset_index()
+
+    df = pd.melt(df, id_vars=["index_country", "index_indicat"])
+    df.dropna(subset=['value'])
+
+    df = df.rename(columns={'index_country': 'id_country', 'index_indicat': 'id_ind', 'variable': 'year'})
+
+    return df
+
 
 if __name__ == "__main__":
-    filter_csv(in_path, out_path)
+    df = filter_indicators(in_file)
+    df = add_countryid_indicatorid(df)
+    df = unpivot_df(df)
+
+    df.to_csv(out_file)
+
