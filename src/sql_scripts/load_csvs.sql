c74b7b6503401ae52f14ac4e441164b3afd1a182
Christos Christidis <48795138+CChristidis@users.noreply.github.com>
2022-03-11 19:05:40 +0200

    Update load_csvs.sql

--- src/sql_scripts/load_csvs.sql
+++ src/sql_scripts/load_csvs.sql
@@ -1,1 +1,16 @@
-
+LOAD DATA LOCAL INFILE 
+  'C:\\Users\\Christos\\Desktop\\MYE30_csv\\country.csv'
+INTO TABLE 
+  country
+CHARACTER SET 
+  latin1 
+FIELDS OPTIONALLY ENCLOSED BY 
+  '"' 
+TERMINATED BY 
+  ','
+LINES TERMINATED BY 
+  '\n' 
+IGNORE 
+  1 
+LINES 
+  (id, Name, Code);
