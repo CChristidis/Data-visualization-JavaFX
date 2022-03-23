66294235a1982c0c1ac6b063f05616551d41629c
Christos Christidis <48795138+CChristidis@users.noreply.github.com>
2022-03-24 00:35:25 +0200

    Delete src/src/main/java directory

--- src/src/main/java/dao/IndicatorDAO.java
+++
@@ -1,9 +0,0 @@
-package main.java.dao;
-
-public interface IndicatorDAO {
-	public int readIndicatorIdFromName(String name);
-	public void delete(String name);
-	public void update(String name);
-	public void create(int id, String name, String code);
-
-}
