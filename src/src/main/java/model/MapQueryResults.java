f00dca97da4bb04f6a4db7f381647f2a9dd97139
Christos Christidis <48795138+CChristidis@users.noreply.github.com>
2022-03-24 16:45:52 +0200

    Delete src/src/main/java directory

--- src/src/main/java/model/MapQueryResults.java
+++
@@ -1,9 +0,0 @@
-package main.java.model;
-
-// TODO: updating hashmap from <List<Integer>, Double> to <List<String>, Double> so that id's will be converted back to names (country, indicator).
-public class MapQueryResults {
-
-	
-	
-
-}
