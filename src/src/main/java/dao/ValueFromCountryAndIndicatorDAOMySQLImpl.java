c971550f9a8e03906e452096b810802b52adf1c8
Christos Christidis <48795138+CChristidis@users.noreply.github.com>
2022-03-24 16:49:26 +0200

    Add files via upload

---
+++ src/src/main/java/dao/ValueFromCountryAndIndicatorDAOMySQLImpl.java
@@ -0,0 +1,74 @@
+package main.java.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ValueFromCountryAndIndicatorDAOMySQLImpl implements ValueFromCountryAndIndicatorDAO {
+	private Connector connection;
+	private ResultSet queryResult;
+	private Map<List<Integer>, Long> indicatorYearlyValueForCountry;
+	
+	public ValueFromCountryAndIndicatorDAOMySQLImpl() {
+		connection = new Connector();
+		indicatorYearlyValueForCountry = new HashMap<List<Integer>, Long>();
+	}
+
+	
+	@Override
+	public Map<List<Integer>, Long> readValueFromCountryAndIndicator(List<Integer> id_country, List<Integer> id_ind, List<Integer> years) {
+		List<Integer> country_indicator_year = new ArrayList<Integer>(Arrays.asList(0, 0, 0));
+		
+ 		for (Integer country: id_country) {
+ 			country_indicator_year.set(0, country);
+			for (Integer indicator: id_ind) {
+				country_indicator_year.set(1, indicator);
+				for (Integer year: years) {
+					country_indicator_year.set(2, year);
+					String query = "SELECT value FROM indicates WHERE id_country = " + country 
+							+ " AND id_ind = " + indicator + " AND year = " + year + ";";
+					//System.out.println(query);
+					long value = 0;
+					try {
+						connection.connectWithDB();
+						queryResult = connection.executeQuery(query);
+						if (queryResult.next()) {
+							value = queryResult.getLong("value");
+							this.indicatorYearlyValueForCountry.put(country_indicator_year, value);
+							//System.out.println(Arrays.toString(indicatorYearlyValueForCountry.entrySet().toArray()));
+						} 
+						
+						}catch (SQLException ex) {
+							connection.perrSQL(ex);
+					 	}
+					country_indicator_year = new ArrayList<Integer>(Arrays.asList(country, indicator, 0));
+				}
+			}
+		}
+ 		// System.out.println(Arrays.toString(indicatorYearlyValueForCountry.entrySet().toArray()));
+ 		return this.indicatorYearlyValueForCountry;
+	}
+
+	@Override
+	public void delete(int id_country, int id_ind, int year) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(int id_country, int id_ind, int year) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void create(int id_country, int id_ind, int year, int value) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
